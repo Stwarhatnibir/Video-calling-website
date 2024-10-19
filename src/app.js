9875e41f92a0d803bc5f39490385d2f7e483b0b6
Joy Sarkar <joysarkar8171@gmail.com>
2024-10-19 08:59:14 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,20 +1,36 @@
 let express = require("express");
-let app = express();
-let server = require("http").Server(app);
-let io = require("socket.io")(server);
-let stream = require("./ws/stream");
+let fs = require("fs");
+let https = require("https");
+let socketIO = require("socket.io");
+let stream = require("./src/ws/stream");
 let path = require("path");
 let favicon = require("serve-favicon");
 
+let app = express();
+
+// SSL certificate options
+let sslOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/server.mgplugins.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/server.mgplugins.com/fullchain.pem')
+};
+
+// Setup middlewares
 app.use(favicon(path.join(__dirname, "favicon.ico")));
 app.use("/assets", express.static(path.join(__dirname, "assets")));
 
+// Route for serving the index.html
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/src/index.html");
 });
 
+// Create HTTPS server
+let httpsServer = https.createServer(sslOptions, app);
+
+// Setup socket.io for WebSocket connections
+let io = socketIO(httpsServer);
 io.of("/stream").on("connection", stream);
 
-server.listen(3000 , "0.0.0.0" , ()=>{
-  console.log("Server Started on port 3000");
+// Listen on port 3000
+httpsServer.listen(3000, () => {
+  console.log("HTTPS server running on port 3000");
 });
